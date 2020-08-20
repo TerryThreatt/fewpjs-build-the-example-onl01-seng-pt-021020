5d75ce7067c26ddadd9818903a30b2ca967594c4
TerryThreatt <terry.threatt@gmail.com>
2020-08-20 16:53:32 -0500

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,41 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', (e) => {
+  //nodes
+  const modal = document.querySelector('#modal');
+  const likes = document.querySelectorAll('.like-glyph');
+  const errMessage = document.querySelector('#modal-message');
 
+  // click
+  likes.forEach((like) => {
+    like.addEventListener('click', (e) => {
+      mimicServerCall('url')
+        .then(() => {
+          if (e.target.innerHTML == EMPTY_HEART) {
+            e.target.innerHTML = FULL_HEART
+            e.target.classList.add('activated-heart')
+          }
+          else {
+            e.target.innerHTML = EMPTY_HEART
+            e.target.classList.remove('activated-heart')
+          }
+        })
+      .catch((error) => {
+
+        setTimeout(() => {
+          errMessage.textContent = error.message
+          modal.classList.remove('hidden')
+        }, 5000)
+
+        modal.className = 'hidden'
+      })
+    })
+  })
+
+
+
+})
 
 
 
